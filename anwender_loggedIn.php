05c18708cb3466fbbb558f722518bb12cf45a0a9
Spiller <dlp@chg-meridian.com>
2019-07-17 15:02:14 +0200

    Start creating functions for user.

--- anwender_loggedIn.php
+++ anwender_loggedIn.php
@@ -9,6 +9,18 @@
     session_start();
     if($_SESSION['U'] == 123321) {
       echo "Wir sind in der anwender_loggedIn.php";
+    ?>
+    <br>
+    <p>Register Student!</p>
+    <a href="register_student.php">
+      <img border="0" alt="Student" src="Student.png">
+    </a>
+    <br>
+    <p>Create Survey!</p>
+    <a href="create_survey.php">
+      <img border="0" alt="Survey" src="Survey.png">
+    </a>
+    <?php
     } else {
       header('Location: index.php');
     }
